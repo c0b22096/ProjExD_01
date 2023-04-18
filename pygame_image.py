c1472537705515f496e3d60557c16429136cbb25
hiromu <c0b2209617@edu.teu.ac.jp>
2023-04-18 15:39:26 +0900

    二問目

--- pygame_image.py
+++ pygame_image.py
@@ -6,6 +6,9 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20230418/fig/pg_bg.jpg")
+    kk_img = pg.image.load("ex01-20230418/fig/3.png")
+    kk_img10 = pg.transform.flip(kk_img, True, False)
+
     tmr = 0
 
     while True:
